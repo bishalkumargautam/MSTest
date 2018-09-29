74e8f570b2ab91f4ee054ecff983c19d459aaf71
Bishal Gautam <bishalkrgautam@gmail.com>
2018-09-28 21:00:33 -0500

    ms test

---
+++ Calculator/UnitTestModule/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestModule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestModule")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a530efa-6682-41e5-aa74-2b142d5c51c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator/UnitTestModule/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Library;
+
+namespace UnitTestModule
+{
+    [TestClass]  
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange Section- we initialize variables and set its values.
+            int expectedResult = 24;
+            int num1 = 20;
+            int num2 = 4;
+
+            //Act Section- We invoke the method we want to test.
+            int actualResult = CalculationOperation.Add(num1, num2);
+
+            //Assert Section- we verify the method that we want to verify if the emthod behaves as per expected
+            Assert.AreEqual(expectedResult, actualResult);
+            Console.WriteLine("This is the test method for addition");
+
+        }
+
+        [TestMethod]
+        public void Subtraction_test()
+        {
+            //Arrange Section- we initialize variables and set its values.
+            int expectedResult = 20;
+            int num1 = 24;
+            int num2 = 4;
+
+            //Act Section- We invoke the method we want to test.
+            int actualResult = CalculationOperation.Substract(num1, num2);
+
+            //Assert Section- we verify the method that we want to verify if the emthod behaves as per expected
+            Assert.AreEqual(expectedResult, actualResult);
+            Console.WriteLine("This is the test method for subtraction");
+
+        }
+        [TestMethod]
+        public void Division_test()
+        {
+            //Arrange Section- we initialize variables and set its values.
+            int expectedResult = 4;
+            int num1 = 24;
+            int num2 = 6;
+
+            //Act Section- We invoke the method we want to test.
+            int actualResult = CalculationOperation.Divide(num1, num2);
+
+            //Assert Section- we verify the method that we want to verify if the emthod behaves as per expected
+            Assert.AreEqual(expectedResult, actualResult);
+            Console.WriteLine("This is the test method for Division test");
+
+        }
+        [TestMethod]
+        public void Multiply_test()
+        {
+            //Arrange Section- we initialize variables and set its values.
+            int expectedResult = 24;
+            int num1 = 6;
+            int num2 = 4;
+
+            //Act Section- We invoke the method we want to test.
+            int actualResult = CalculationOperation.Multiply(num1, num2);
+
+            //Assert Section- we verify the method that we want to verify if the emthod behaves as per expected
+            Assert.AreEqual(expectedResult, actualResult);
+            Console.WriteLine("This is the test method for addition");
+
+        }
+    }
+}
